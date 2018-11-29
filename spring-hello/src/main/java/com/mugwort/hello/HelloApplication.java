be32dc9082000a781d6ce9b3edf0b6c813e9c1fb
aiye <aiye@myweimai.com>
2018-11-29 12:23:51 +0800

    spring command hello world  commit.

---
+++ spring-hello/src/main/java/com/mugwort/hello/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.mugwort.hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello world");
+    }
+}
